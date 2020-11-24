e19771496fb37637d10fd64e3ba2c4819f4c0642
mykola_melnyk <kolanio757@gmail.com>
2020-11-25 00:41:40 +0200

    add Diagnostic Test Problem Solver

---
+++ DiagnosticTest/Diagnostic.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace DiagnosticTest
+{
+    class Diagnostic
+    {
+        private double TP, FP, FN, TN, L12, L21;
+        private double SE, SP, w, P, lowerLimit, higherLimit;
+        private bool qualitative;
+
+        public Diagnostic(double tp = 467.0, double fp=107.0, double fn=95.0, double tn=3468.0, double l12=100.0, double l21 = 5.0 )
+        {
+            TP = tp;
+            FP = fp;
+            FN = fn;
+            TN = tn;
+            L12 = l12;
+            L21 = l21;
+        }
+
+        public void Calculate()
+        {
+            //розрахунок чутливості
+            SE = TP/(TP+FN);
+            //розрахунок специфічності
+            SP = TN/(TN+FP);
+            // преваленс
+            double positive = TP + FN;
+            double negative = FP + TN;
+            P = positive / (positive + negative);
+            //співвідношення втрат
+            this.w = L12 / L21;
+            //перевірка корисності
+            qualitative = P * (1 + w) < 1 ? (1 - P) * (1 - SP) < w * P * SE
+                : (1 - P) * SP > w * P * (1 - SE);
+            // визначення інтервалу допустимого співвідношення втрат для тесту
+            lowerLimit = (1 - P) * (1 - SP) / (P * SE);
+            higherLimit = (1 - P) * SP / (P * (1 - SE));
+        }
+
+        public override string ToString()
+        {
+            var answer = new StringBuilder();
+
+            answer.AppendLine("Розв'язок задачі діагностичного тестування:");
+            // чутливість
+            answer.AppendLine($"Чутливість: {SE:0.####}");
+            // специфічність
+            answer.AppendLine($"Специфічність: {SP:0.####}");
+            // преваленс
+            answer.AppendLine($"Преваленс: {P:0.####}");
+            // співвідношення втрат
+            answer.AppendLine($"Cпіввідношення втрат: {w:0.####}");
+            // якість тесту
+            answer.AppendLine($"Тест якісний?: {(qualitative ? "Так" : "Ні")}");
+            // інтервал допустимого співвідношення втрат для тесту
+            answer.AppendLine($"Інтервал допустимого співвідношення втрат для тесту: {lowerLimit:0.####}<=w<={higherLimit:0.####}");
+
+            return answer.ToString();
+        }
+    }
+}
---
+++ DiagnosticTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DiagnosticTest
+{
+    class Program
+    {
+        private static double _tp, _fp, _fn, _tn, _l1, _l2;
+
+        static void Main()
+        {
+            //input martix
+            GetMatrix();
+            // втрата від пропуску цілі
+            GetNegativeLoss();
+            // хибна тривога
+            GetPositiveLoss();
+            var diagnostic = new Diagnostic(_tp, _fp, _fn, _tn, _l1, _l2);
+            diagnostic.Calculate();
+            Console.Write(diagnostic.ToString());
+        }
+
+        private static void GetNegativeLoss()
+        {
+            Console.Write("Введіть втрату для хибно негативного результату L1: ");
+            Console.Write("\n");
+            _l1 = double.Parse(Console.ReadLine() ?? "1.0");
+        }
+
+        private static void GetPositiveLoss()
+        {
+            Console.Write("Введіть втрату для хибно негативного результату L2: ");
+            Console.Write("\n");
+            _l2 = double.Parse(Console.ReadLine() ?? "1.0");
+        }
+
+        private static void GetMatrix()
+        {
+            Console.Write("Введіть TP: ");
+            Console.Write("\n");
+            _tp = double.Parse(Console.ReadLine() ?? "1.0");
+            Console.Write("Введіть FP: ");
+            Console.Write("\n");
+            _fp = double.Parse(Console.ReadLine() ?? "1.0");
+            Console.Write("Введіть FN: ");
+            Console.Write("\n");
+            _fn = double.Parse(Console.ReadLine() ?? "1.0");
+            Console.Write("Введіть TN: ");
+            Console.Write("\n");
+            _tn = double.Parse(Console.ReadLine() ?? "1.0");
+        }
+    }
+}
